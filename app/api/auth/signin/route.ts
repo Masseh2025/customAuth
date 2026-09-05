fae775e7b86d157b318dfc2d1da429397175dbb2
Masseh Ziaie <mas154572@gmail.com>
2026-09-05 01:47:53 -0400

    made it that it added cookie session

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -1,7 +1,8 @@
 import { db } from "@/db";
-import { users } from "@/db/schema";
+import { sessions, users } from "@/db/schema";
 import argon2 from "argon2";
 import { eq } from "drizzle-orm";
+import { cookies } from "next/headers";
 export async function POST(req: Request) {
   // Get req for email and password
 
@@ -17,11 +18,34 @@ export async function POST(req: Request) {
     );
 
   const isCorrect = await argon2.verify(user.passwordHash, password);
-  console.log(isCorrect);
+
+  if (!isCorrect) return Response.json({ message: "Inavlid user or password" });
 
   //Create session
+  const expiresAt = new Date();
+  expiresAt.setDate(expiresAt.getDate() + 7);
+
+  const [session] = await db
+    .insert(sessions)
+    .values({
+      userId: user.id,
+      expiresAt,
+    })
+    .returning();
+
+  // Create session cookie
+
+  const cookieStore = await cookies();
+
+  cookieStore.set("session", session.id, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    path: "/",
+    expires: expiresAt,
+  });
 
   // Send response
   console.log(email, password);
-  return Response.json({ email, password, isCorrect });
+  return Response.json({ message: "login succsesfull dawg" });
 }
